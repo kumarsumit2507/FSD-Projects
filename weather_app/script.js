04a5434d6fcff55364587fe0718ee8960193b39a
Sumit Kumar <902sumitkumar@gmail.com>
2026-08-29 10:46:08 +0530

    Update API key to a placeholder
    Replaced the actual API key with a placeholder.

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,4 +1,4 @@
-const apiKey = "bc9bf103b7e6031b55de08a27284be6e";
+const apiKey = "[API KEY]";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
